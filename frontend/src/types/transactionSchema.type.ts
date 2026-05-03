1ce1539eb9930dfa576513f94b6df93d306542d7
Meki Hallil <107886787+mekihallil@users.noreply.github.com>
2026-05-03 20:27:46 +0300

    feat: create valid schemafor add transaction

---
+++ frontend/src/types/transactionSchema.type.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const transactionSchema = z.object({
+  title: z.string().min(3, "Title must be at least 3 characters"),
+  amount: z.coerce.number().positive("Amount must be a positive number"),
+  type: z.enum(["income", "expense"]),
+  category: z.string().min(1, "Please select a category"),
+});
+
+export type TransactionFormData = z.infer<typeof transactionSchema>;
