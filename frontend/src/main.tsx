e1d196c5728e18b3aaaebb293c5c1e7aee6d42fe
Meki Hallil <107886787+mekihallil@users.noreply.github.com>
2026-05-09 12:46:41 +0300

    feat: implement index style and update main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,21 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
+import { Toaster } from "sonner";
 import { ThemeProvider } from "./components/theme";
 import "./index.css";
 import { router } from "./router";
 
 const queryClient = new QueryClient();
+const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider storageKey="vite-hi-theme">
+    <Toaster
+      theme={isDark ? "dark" : "light"}
+      position="top-center"
+      richColors
+    />
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
     </QueryClientProvider>
