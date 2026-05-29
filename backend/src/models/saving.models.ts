725b54e3096450d04f198a5d9cb26bcba1263630
Meki Hallil <107886787+mekihallil@users.noreply.github.com>
2026-05-29 21:38:19 +0300

    feat: define mongoose models for saving logic

---
+++ backend/src/models/saving.models.ts
@@ -0,0 +1,20 @@
+import { Schema } from "mongoose";
+import type { SInterface } from "../interface/saving.interface.js";
+
+const SavingSchema: Schema<SInterface> = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      maxLength: [50, "name cannot be more than 50 characters "],
+      trim: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    amount: { type: Number, required: true, trim: true },
+    date: { type: Date, required: true, default: Date.now },
+  },
+  { timestamps: true },
+);
