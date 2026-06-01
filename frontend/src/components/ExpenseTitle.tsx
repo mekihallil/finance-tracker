7f2497e2ba4715b7940114b59360ed0e1f1994c8
Meki Hallil <107886787+mekihallil@users.noreply.github.com>
2026-06-01 20:04:22 +0300

    refactor: refactor related components

--- frontend/src/components/ExpenseTitle.tsx
+++ frontend/src/components/ExpenseTitle.tsx
@@ -4,7 +4,7 @@ export const ExpenseTitle: FC = (): ReactElement => {
   return (
     <>
       <article>
-        <div className="p-6">
+        <div className="py-6">
           <p className="text-[26px] font-semibold">Expense Tracker</p>
           <p className="text-[18px] text-[#64748B]">
             Track your daily expenses and spending patterns
--- frontend/src/page/Saving.tsx
+++ frontend/src/page/Saving.tsx
@@ -1,10 +1,15 @@
+import { AddSaving } from "@/components/savingComponents/addSaving";
+import { SavingTitle } from "@/components/savingComponents/SavingTitle";
 import type { FC, ReactElement } from "react";
 
 export const Saving: FC = (): ReactElement => {
   return (
     <>
       <section>
-        <div className="ml-80 mr-10">Saving Page</div>
+        <div className="ml-80 mr-10">
+          <SavingTitle />
+          <AddSaving />
+        </div>
       </section>
     </>
   );
