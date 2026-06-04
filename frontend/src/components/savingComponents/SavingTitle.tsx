8f3ad8c61170de6159d863898a73b566ba2e88f4
Meki Hallil <107886787+mekihallil@users.noreply.github.com>
2026-06-04 20:58:34 +0300

    feat(saving): add button for new goal creation

--- frontend/src/components/savingComponents/SavingTitle.tsx
+++ frontend/src/components/savingComponents/SavingTitle.tsx
@@ -1,15 +1,20 @@
+import { Plus } from "lucide-react";
 import type { FC, ReactElement } from "react";
 
 export const SavingTitle: FC = (): ReactElement => {
   return (
     <>
-      <article>
+      <article className="flex items-center justify-between">
         <div className="py-6">
           <p className="text-[26px] font-semibold">Savings Progress</p>
           <p className="text-[18px] text-[#64748B]">
             Track your savings goals and earn rewards
           </p>
         </div>
+        <section className="flex gap-3">
+          <Plus size={18} className="my-auto"/>
+          <p>Add Goal</p>
+        </section>
       </article>
     </>
   );
