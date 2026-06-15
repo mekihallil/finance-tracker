1984b3554f2969111d1a0ead95c914d6a3c2c9f5
Meki Hallil <107886787+mekihallil@users.noreply.github.com>
2026-06-15 22:36:20 +0300

    feat(saving): connect progress metrics to frontend
    - display percentageSaving, currentSavings, goalSavings, and remainingSaving
    - bind getSavingProgressQuery.data to progress card UI
    - reflect live backend calculations on the frontend

--- frontend/src/components/savingComponents/OverallProgress.tsx
+++ frontend/src/components/savingComponents/OverallProgress.tsx
@@ -1,7 +1,18 @@
+import { useSaving } from "@/hook/userSaving.hook";
 import { Target } from "lucide-react";
 import type { FC, ReactElement } from "react";
 
 export const OverallProgress: FC = (): ReactElement => {
+  const GetSavings = useSaving();
+  const savings = GetSavings;
+
+  if (savings.isLoading) {
+    return <div>... loading</div>;
+  }
+  if (savings.isError) {
+    return <div>error</div>;
+  }
+  const data = savings.data;
   return (
     <>
       <article className="rounded-[20px] dark:bg-[#2C3546] border border-gray-300 dark:border-none p-7 mb-7">
@@ -10,12 +21,14 @@ export const OverallProgress: FC = (): ReactElement => {
           <h2 className="font-medium ">Overall Progress</h2>
         </header>
         <section className="flex justify-between py-7  ">
-          <h1 className="font-semibold text-3xl">$3600.00</h1>
-          <h1 className="text-xl text-gray-400">of $6500.00</h1>
+          <h1 className="font-semibold text-3xl">
+            ${data.currentSavings || 0}
+          </h1>
+          <h1 className="text-xl text-gray-400">of ${data.goalSavings || 0}</h1>
         </section>
         <section className="flex justify-between pt-3 text-gray-400">
-          <h1>78.3% complete</h1>
-          <h1>$2900.00 to go</h1>
+          <h1>{data.percentageSaving || 0}% complete</h1>
+          <h1>${data.remainingSaving || 0} to go</h1>
         </section>
       </article>
     </>
--- frontend/src/services/saving.service.ts
+++ frontend/src/services/saving.service.ts
@@ -2,7 +2,7 @@ import { apiClient } from "./api.service";
 
 export const savingService = {
   getGoalSaving: async () => {
-    const { data } = await apiClient.get("/getsaving");
+    const { data } = await apiClient.get("/saving/getsaving");
     return data;
   },
 };
