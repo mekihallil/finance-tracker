3075ce36716e8884a02213819476a56602d2b3bf
Meki Hallil <107886787+mekihallil@users.noreply.github.com>
2026-07-01 18:06:56 +0300

    feat: prevent additional top-ups once a saving goal is 100% complete

--- frontend/src/components/savingComponents/AddMoneyPoppver.tsx
+++ frontend/src/components/savingComponents/AddMoneyPoppver.tsx
@@ -21,10 +21,12 @@ import { useEffect, useState } from "react";
 
 type AddMoneyPopoverProps = {
   savingId: string;
+  isComplete: number;
 };
 
 export const AddMoneyPopover: FC<AddMoneyPopoverProps> = ({
   savingId,
+  isComplete,
 }): ReactElement => {
   const { AddMoney } = useSaving();
   const [open, setOpen] = useState(false);
@@ -58,6 +60,7 @@ export const AddMoneyPopover: FC<AddMoneyPopoverProps> = ({
       },
     );
   };
+
   return (
     <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger asChild>
@@ -65,48 +68,52 @@ export const AddMoneyPopover: FC<AddMoneyPopoverProps> = ({
           <DollarSign size={15} className="my-auto" /> Add Money
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-80">
-        <div className="grid gap-4">
-          <div className="space-y-2">
-            <h4 className="leading-none font-medium">
-              Enter contribution amount:
-            </h4>
-          </div>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="grid gap-2">
-              <div className="grid grid-cols-3 items-center gap-4">
-                <Label htmlFor="amount">Amount</Label>
-                <Input
-                  id="amount"
-                  type="number"
-                  step="0.01"
-                  min={0}
-                  placeholder="0.00"
-                  className="col-span-2 h-8"
-                  {...register("amount", { valueAsNumber: true })}
-                />
+      <section>
+        {!isComplete && (
+          <PopoverContent className="w-80">
+            <div className="grid gap-4">
+              <div className="space-y-2">
+                <h4 className="leading-none font-medium">
+                  Enter contribution amount:
+                </h4>
               </div>
-              {errors.amount && (
-                <span className="text-xs text-red-500 font-medium">
-                  {errors.amount.message}
-                  {toast.error(`${errors.amount.message}`)}
-                </span>
-              )}
-              <Button
-                disabled={AddMoney.isPending}
-                type="submit"
-                variant="outline"
-              >
-                {AddMoney.isPending ? (
-                  <Loader2 size={15} className="animate-spin" />
-                ) : (
-                  "Submit"
-                )}
-              </Button>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="grid gap-2">
+                  <div className="grid grid-cols-3 items-center gap-4">
+                    <Label htmlFor="amount">Amount</Label>
+                    <Input
+                      id="amount"
+                      type="number"
+                      step="0.01"
+                      min={0}
+                      placeholder="0.00"
+                      className="col-span-2 h-8"
+                      {...register("amount", { valueAsNumber: true })}
+                    />
+                  </div>
+                  {errors.amount && (
+                    <span className="text-xs text-red-500 font-medium">
+                      {errors.amount.message}
+                      {toast.error(`${errors.amount.message}`)}
+                    </span>
+                  )}
+                  <Button
+                    disabled={AddMoney.isPending}
+                    type="submit"
+                    variant="outline"
+                  >
+                    {AddMoney.isPending ? (
+                      <Loader2 size={15} className="animate-spin" />
+                    ) : (
+                      "Submit"
+                    )}
+                  </Button>
+                </div>
+              </form>
             </div>
-          </form>
-        </div>
-      </PopoverContent>
+          </PopoverContent>
+        )}
+      </section>
     </Popover>
   );
 };
