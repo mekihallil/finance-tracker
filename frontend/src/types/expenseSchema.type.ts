0888cbb6c46a4104dd107485fa8c184dea760477
Meki Hallil <107886787+mekihallil@users.noreply.github.com>
2026-07-04 20:12:33 +0300

    feat(types): add extended expense schema with ID/Date and migrate float64 to number

--- frontend/src/types/expenseSchema.type.ts
+++ frontend/src/types/expenseSchema.type.ts
@@ -3,13 +3,18 @@ import { z } from "zod";
 export const expenseSchema = z.object({
   title: z.string().min(3, "Title must be at least 3 characters"),
   amount: z
-    .float64({ error: "Please enter a valid number" })
+    .number({ error: "Please enter a valid number" })
     .positive("Amount must be a positive number"),
   type: z.enum(["income", "expense"]),
   category: z
     .string({ error: "Please select a category" })
     .min(1, "Please select a category"),
+});
 
+export const expenseSchemaWithIdAndDate = expenseSchema.extend({
+  _id: z.string(),
+  createAt: z.coerce.date(),
 });
 
 export type ExpenseFormData = z.infer<typeof expenseSchema>;
+export type ExpenseFormDataWithId = z.infer<typeof expenseSchemaWithIdAndDate>;
