6c109b2d4bd88f3daf99666dca227b3f23a273db
Meki Hallil <107886787+mekihallil@users.noreply.github.com>
2026-07-28 21:18:48 +0300

    feat: create splitTitle component for bill splitting page

---
+++ frontend/src/components/split-bill/SavingTitle.tsx
@@ -0,0 +1,22 @@
+import { Plus } from "lucide-react";
+import { type FC, type ReactElement } from "react";
+
+export const SplitTitle: FC = (): ReactElement => {
+  return (
+    <article className="flex items-center justify-between">
+      <div className="py-6">
+        <p className="text-[26px] font-semibold">Split Bills</p>
+        <p className="text-[18px] text-[#64748B]">
+          Split expenses with friends and track payments
+        </p>
+      </div>
+      <button
+        type="button"
+        className="flex gap-3 border rounded-2xl px-4 py-2 hover:-translate-y-1 "
+      >
+        <Plus size={18} className="my-auto" aria-hidden="true" />
+        <span>New Split</span>
+      </button>
+    </article>
+  );
+};
