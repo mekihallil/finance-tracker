4bc6d0e3f49f977abfd446a88f2e8d3dd319e215
Meki Hallil <107886787+mekihallil@users.noreply.github.com>
2026-07-28 21:22:01 +0300

    feat: integrate splitTitle component into splitBill page

--- frontend/src/page/SplitBill.tsx
+++ frontend/src/page/SplitBill.tsx
@@ -1,9 +1,12 @@
+import { SplitTitle } from "@/components/split-bill/SavingTitle";
 import { type FC, type ReactElement } from "react";
 
 export const SplitBill: FC = (): ReactElement => {
   return (
     <section className="bg-[#FAFAFB] dark:bg-[#11161D]">
-      <div className="ml-80 mr-10">split-bill</div>
+      <div className="ml-80 mr-10">
+        <SplitTitle />
+      </div>
     </section>
   );
 };
