44fb8a9017f706c0a3f18346e691e5e6ccd26f76
Meki Hallil <107886787+mekihallil@users.noreply.github.com>
2026-07-28 21:13:20 +0300

    feat(ui): create SplitBill page component

---
+++ frontend/src/page/SplitBill.tsx
@@ -0,0 +1,9 @@
+import { type FC, type ReactElement } from "react";
+
+export const SplitBill: FC = (): ReactElement => {
+  return (
+    <section className="bg-[#FAFAFB] dark:bg-[#11161D]">
+      <div className="ml-80 mr-10">split-bill</div>
+    </section>
+  );
+};
