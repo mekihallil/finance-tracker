edf0d74c85ffeb63ec196ba52b1be48cf6c4789f
Meki Hallil <107886787+mekihallil@users.noreply.github.com>
2026-08-08 22:15:32 +0300

    refactor: rename BalanceUtils to BalanceSummary
    Rename file and function/component from BalanceUtils to
    BalanceSummary for clearer naming.

---
+++ frontend/src/components/split-bill/BalanceSummary.tsx
@@ -0,0 +1,44 @@
+import { Calculator, DollarSign } from "lucide-react";
+import type { FC, ReactElement } from "react";
+
+type Utils = {
+  name: string;
+  icon: ReactElement;
+  amount: number;
+};
+
+const utils: Utils[] = [
+  {
+    name: "You Owe",
+    icon: <DollarSign />,
+    amount: 3.24,
+  },
+  {
+    name: "Owed to You",
+    icon: <DollarSign />,
+    amount: 60.25,
+  },
+  {
+    name: "Net Balance",
+    icon: <Calculator />,
+    amount: 60.25,
+  },
+];
+
+export const BalanceSummary: FC = (): ReactElement => {
+  return (
+    <main className="grid grid-cols-3 gap-6.25">
+      {utils.map(({ name, icon, amount }) => {
+        return (
+          <section className="flex gap-3.5 rounded-3xl bg-[#2C3546] p-5.5">
+            <section className="my-auto">{icon}</section>
+            <section>
+              <h1>{name}</h1>
+              <h1 className="text-2xl font-semibold">${amount}</h1>
+            </section>
+          </section>
+        );
+      })}
+    </main>
+  );
+};
