ae6fe8daf207c6eb5dd7e0a622861ae120d954af
Meki Hallil <107886787+mekihallil@users.noreply.github.com>
2026-09-08 22:18:44 +0300

    refactor: update router imports to use renamed AddSplit controller

--- backend/src/routes/split.route.ts
+++ backend/src/routes/split.route.ts
@@ -2,11 +2,11 @@ import { Router } from "express";
 
 import { validate } from "../middlewares/validate.middlewares.js";
 import { splitValidatorSchema } from "../validations/split.validation.js";
-import { createSplit, getSplit } from "../controllers/split.controller.js";
+import { AddSplit, getSplit } from "../controllers/split.controller.js";
 
 const router = Router();
 
-router.post("/create", validate(splitValidatorSchema), createSplit);
+router.post("/create", validate(splitValidatorSchema), AddSplit);
 router.get("/splitBills", getSplit);
 
 export default router;
